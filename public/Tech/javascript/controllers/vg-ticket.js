20fc7e0d7fcb2e913eda7f67c3189861bb3bcb25
cvogel3434 <cvogel.a.m@gmail.com>
2023-02-02 19:24:21 -0600

    test email call

--- public/Tech/javascript/controllers/vg-ticket.js
+++ public/Tech/javascript/controllers/vg-ticket.js
@@ -6,6 +6,8 @@ import {SYNCticket, STARTticket} from '/Tech/javascript/tools/vapi-FTrequest.js'
 import {ServiceTicket} from '/Tech/javascript/controllers/ticket/service-ticket.js';
 import {ServicePresentation} from '/Tech/javascript/controllers/ticket/service-presentation.js';
 
+import {SENDrequestvhp} from 'https://www.vhpportal.com/repo/apis/vapi/vapicore.js';
+
 let publicfolder = '/Tech/bin/css'; //not sure we need
 // Load Data //
 
@@ -114,6 +116,21 @@ var qactions = {
   }
 };
 var mactions = {
+  email:{
+    id:'test-email',
+    src:'https://www.vhpportal.com/repo/assets/icons/disk.png'
+    onclick:(ele)=>{
+
+      apitest.SENDrequestvhp({
+        to:'christianv@vogelheating.com',
+        subject:'Check This Out',
+        attach:{wonum:'<div>Hello</div>',checks:['<div>hello</div>'],invoice:'<div>hello</div>'}
+      },
+      'MAIL',{}).then(
+        answer=>{console.log(answer);}
+      );
+    }
+  },
   save:{
     id:'wo-save-button',
     src:'https://www.vhpportal.com/repo/assets/icons/disk.png',
@@ -203,5 +220,5 @@ var TOGGLErepairtable = (hide=undefined) => {
 window.UPDATErepairapproval = (data)=> {
   //console.log("TICKET:",ticket.ticket.repairs==data.repairs)
   ticket.ticket = data;
-  
+
 }
